09f76d0bc48a59b6c9627d89d29eff907526e38a
YiguiDing <2449695354@qq.com>
2021-12-29 22:10:20 +0800

    解决提交POST发送Email的跳转问题，解决显示几分钟读完的问题，删除百度分享图标

--- js/main.js
+++ js/main.js
@@ -100,4 +100,67 @@ $(document).ready(function () {
   });
 
 
+  $(".c-submit_btn").click(
+    function ()
+    {
+      var PostURL=document.getElementById('POST_URL').value;
+      var Redirect_URL=document.getElementById('Redirect_URL').value;
+      var data=
+      {
+        name:     document.getElementById('form-name').value,
+        email:    document.getElementById('form-email').value,
+        tittle:   document.getElementById('form-subject').value,
+        context:  document.getElementById('form-text').value
+      };      
+      if(data["name"]=="")
+      {
+        alert("姓名为必填字段");
+      }   
+      else if(data["email"].indexOf("@")==-1)
+      {
+        alert("请正确填写您的Email地址");
+      }
+      else if(data["context"]=="")
+      {
+        alert("请填写邮件内容");
+      }
+      else
+      {
+        // $.post(
+        //   PostURL,
+        //   data,
+        //   function(data,status){
+        //     alert("即将重定向");
+        //     window.location.replace(Redirect_URL);
+        //   }
+        // );
+
+        //上述函数是本函数的简写，返回值为302时被视为错误，于是回调函数不能执行不能重定向，只能用下列函数
+        $.ajax(
+          {
+            url:              PostURL,
+            type:             'POST',
+            data:             data,
+            crossDomain:      true,                                     //允许跨域
+            //dataType:       'jsonp',
+            success:          function(result,status,xhr)
+                              {
+                                //alert("请求成功");
+                              },
+            error:            function(xhr,status,error)
+                              {
+                                //alert('请求失败');
+                              },          
+            complete:         function(xhr,status)//因为收到302重定向后被视为错误，所以执行下面的
+                              {
+                                //alert('无论请求成功失败或失败都执行跳转');
+                                window.location.replace(Redirect_URL);
+                              }
+          }
+        );
+        
+      }
+    }
+  );
+
 });
