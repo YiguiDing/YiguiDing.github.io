83fba495c00039d21efd98bcd7b6ac0efc84c68e
YiguiDing <50746494+YiguiDing@users.noreply.github.com>
2023-04-20 19:53:54 +0800

    Delete assets directory

--- assets/index.911f20ef.js
+++
@@ -1,1 +0,0 @@
-import{D as c}from"./index.c38bca4f.js";import{_,r as o,o as p,j as l,w as i,f as t}from"./index.a5abefe3.js";const f={components:{Default:c}};function m(d,u,C,x,g,B){const e=o("GitCalendar"),n=o("Tags"),a=o("Categories"),r=o("ArticalOrderBy"),s=o("Default");return p(),l(s,null,{default:i(()=>[t(e),t(n),t(a),t(r)]),_:1})}const h=_(f,[["render",m]]);export{h as default};
