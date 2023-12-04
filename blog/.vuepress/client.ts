f3cce3aa7e09b6bd3bd60cf83820653db5def37c
YiguiDing <2449695354@qq.com>
2023-12-04 14:09:37 +0800

    update: 添加自己写的小游戏组件

--- blog/.vuepress/client.ts
+++ blog/.vuepress/client.ts
@@ -1,9 +1,10 @@
 import { defineClientConfig } from "@vuepress/client";
 import Live2D from "./components/Live2D.vue";
+import Spacecraft from "./components/Spacecraft.vue";
 
 export default defineClientConfig({
   enhance({ app, router, siteData }) {},
   setup() {},
   layouts: {},
-  rootComponents: [Live2D],
+  rootComponents: [Live2D, Spacecraft],
 });
---
+++ blog/.vuepress/components/Spacecraft.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import "spacecraft.io/client/style.css";
+import { shallowRef, onMounted } from "vue";
+// @ts-ignore
+import type { ServerConfig } from "spacecraft.io/client";
+let list: ServerConfig = [
+  {
+    label: "香港:https://dingdingdang.online",
+    value: "https://dingdingdang.online",
+  },
+];
+let Spacecraft = shallowRef();
+onMounted(async () => {
+  // @ts-ignore
+  const { SpacecraftVue } = await import("spacecraft.io/client");
+  Spacecraft.value = SpacecraftVue;
+});
+</script>
+<template>
+  <component v-if="Spacecraft" :is="Spacecraft" :server-addrs="list" />
+</template>
