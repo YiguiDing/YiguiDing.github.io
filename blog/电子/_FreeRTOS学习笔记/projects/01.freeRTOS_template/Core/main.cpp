4f900bf1e4d53fdfb3acd9bd9366436ad192b343
YiguiDing <2449695354@qq.com>
2024-08-29 17:50:10 +0800

    update: 01.freeRTOS_template add TaskCreateStatic example

--- blog/电子/_FreeRTOS学习笔记/projects/01.freeRTOS_template/Core/main.cpp
+++ blog/电子/_FreeRTOS学习笔记/projects/01.freeRTOS_template/Core/main.cpp
@@ -9,6 +9,7 @@
 /* Driver includes. */
 #include <Serial.h>
 /*-----------------------------------------------------------*/
+TaskHandle_t task1Handler;
 void task1(void *parameters)
 {
   Serial_Printf("vTaskStartScheduler\n");
@@ -22,6 +23,7 @@ void task1(void *parameters)
     vTaskDelay(100);        /* delay 100 ticks */
   }
 }
+TaskHandle_t task2Handler;
 void task2(void *parameters)
 {
   /* Unused parameters. */
@@ -33,6 +35,21 @@ void task2(void *parameters)
     vTaskDelay(100);        /* delay 100 ticks */
   }
 }
+#define TASK3_STACK_SIZE 100
+StackType_t task3Stack[TASK3_STACK_SIZE];
+StaticTask_t xTask3TCB;
+TaskHandle_t task3Handler;
+void task3(void *parameters)
+{
+  /* Unused parameters. */
+  (void)parameters;
+
+  for (;;)
+  {
+    Serial_Printf("333\n"); /* Example Task Code */
+    vTaskDelay(100);        /* delay 100 ticks */
+  }
+}
 /*-----------------------------------------------------------*/
 
 int main(void)
@@ -40,14 +57,12 @@ int main(void)
   Serial_Init();
   Serial_Printf("Example FreeRTOS Project0\n"); // 有输出
 
-  // task1
-  TaskHandle_t pxCreatedTask1Handler;
-  xTaskCreate(task1, "task1", 1024, NULL, 1, &pxCreatedTask1Handler); // 为什么卡死
-  Serial_Printf("Example FreeRTOS Project1\n"); // 没有输出
-
-  // task2
-  TaskHandle_t pxCreatedTask2Handler;
-  xTaskCreate(task2, "task2", 100, NULL, 1, &pxCreatedTask2Handler);
+  // task1 动态分配栈
+  xTaskCreate(task1, "task1", 1024, NULL, 1, &task1Handler);
+  // task2 动态分配栈
+  xTaskCreate(task2, "task2", 100, NULL, 1, &task2Handler);
+  // task3 静态分配栈
+  task3Handler = xTaskCreateStatic(task3, "task3", TASK3_STACK_SIZE, NULL, 1, task3Stack, &xTask3TCB);
 
   /* Start the scheduler. */
   vTaskStartScheduler();
@@ -73,3 +88,16 @@ void vApplicationStackOverflowHook(TaskHandle_t xTask,
 
 #endif /* #if ( configCHECK_FOR_STACK_OVERFLOW > 0 ) */
 /*-----------------------------------------------------------*/
+#if (configSUPPORT_STATIC_ALLOCATION > 0)
+#define IdleTaskStackSize 100
+StaticTask_t xIdleTaskTCB;
+StackType_t xIdleTaskStack[IdleTaskStackSize];
+void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer,
+                                   StackType_t **ppxIdleTaskStackBuffer,
+                                   uint32_t *pulIdleTaskStackSize)
+{
+  *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
+  *ppxIdleTaskStackBuffer = xIdleTaskStack;
+  *pulIdleTaskStackSize = IdleTaskStackSize;
+}
+#endif /* #if ( configSUPPORT_STATIC_ALLOCATION > 0 ) */
--- blog/电子/_FreeRTOS学习笔记/projects/01.freeRTOS_template/Library/FreeRTOSConfig.h
+++ blog/电子/_FreeRTOS学习笔记/projects/01.freeRTOS_template/Library/FreeRTOSConfig.h
@@ -42,19 +42,21 @@
  * See http://www.freertos.org/a00110.html
  *----------------------------------------------------------*/
 
-#define configUSE_PREEMPTION		1
-#define configUSE_IDLE_HOOK			0
-#define configUSE_TICK_HOOK			0
-#define configCPU_CLOCK_HZ			( ( unsigned long ) 72000000 )
-#define configTICK_RATE_HZ			( ( TickType_t ) 1000 )
-#define configMAX_PRIORITIES		( 5 )
-#define configMINIMAL_STACK_SIZE	( ( unsigned short ) 128 )
-#define configTOTAL_HEAP_SIZE		( ( size_t ) ( 15 * 1024 ) )
-#define configMAX_TASK_NAME_LEN		( 16 )
-#define configUSE_TRACE_FACILITY	0
-#define configUSE_16_BIT_TICKS		0
-#define configIDLE_SHOULD_YIELD		1
-#define configUSE_MUTEXES			1
+#define configUSE_PREEMPTION		                1
+#define configUSE_IDLE_HOOK			                0
+#define configUSE_TICK_HOOK			                0
+#define configCPU_CLOCK_HZ			                ( ( unsigned long ) 72000000 )
+#define configTICK_RATE_HZ			                ( ( TickType_t ) 1000 )
+#define configMAX_PRIORITIES		                ( 5 )
+#define configMINIMAL_STACK_SIZE	                ( ( unsigned short ) 128 )
+#define configTOTAL_HEAP_SIZE		                ( ( size_t ) ( 15 * 1024 ) )
+#define configMAX_TASK_NAME_LEN		                ( 16 )
+#define configUSE_TRACE_FACILITY	                0
+#define configUSE_16_BIT_TICKS		                0
+#define configIDLE_SHOULD_YIELD		                1
+#define configUSE_MUTEXES			                1
+#define configSUPPORT_STATIC_ALLOCATION			    1
+
 
 
 /* Set the following definitions to 1 to include the API function, or zero
