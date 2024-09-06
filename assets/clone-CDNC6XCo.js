98f37ea3b286182ef820220c9da57969414bda29
YiguiDing <50746494+YiguiDing@users.noreply.github.com>
2024-09-06 03:49:40 +0000

    Deploying to static-pages from @ YiguiDing/YiguiDing.github.io@f9e829d53df0b7e33c423dff64cd15e2ae4d014d 🚀

--- assets/clone-CDNC6XCo.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./graph-CQD2ikBV.js";var a=4;function n(o){return r(o,a)}export{n as c};
---
+++ assets/nginx实现反向代理并启用ssl的最简配置.html-rAlbBHhN.js
@@ -0,0 +1,41 @@
+import{_ as s,c as n,o as a,b as e}from"./app-DOw_hxf4.js";const i={},l=e(`<h1 id="nginx实现反向代理并启用ssl的最简配置" tabindex="-1"><a class="header-anchor" href="#nginx实现反向代理并启用ssl的最简配置"><span>nginx实现反向代理并启用ssl的最简配置</span></a></h1><div class="language-confg line-numbers-mode" data-highlighter="shiki" data-ext="confg" data-title="confg" style="--shiki-light:#4c4f69;--shiki-dark:#E6E6E6;--shiki-light-bg:#eff1f5;--shiki-dark-bg:#222222;"><pre class="shiki shiki-themes catppuccin-latte slack-dark vp-code"><code><span class="line"><span>server {</span></span>
+<span class="line"><span>    listen       8082; </span></span>
+<span class="line"><span>    server_name  localhost;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    #启用ssl</span></span>
+<span class="line"><span>    ssl on;</span></span>
+<span class="line"><span>    #ssl证书</span></span>
+<span class="line"><span>    ssl_certificate  /home/dyg/.ssl/cert.crt;</span></span>
+<span class="line"><span>    #ssl证书私钥</span></span>
+<span class="line"><span>    ssl_certificate_key /home/dyg/.ssl/cert-key.key;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    location / {</span></span>
+<span class="line"><span>        #反向代理</span></span>
+<span class="line"><span>        proxy_pass http://localhost:8081;</span></span>
+<span class="line"><span>        #默认页面</span></span>
+<span class="line"><span>        index  index.html index.htm;</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span>}</span></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h1 id="将http请求转成https" tabindex="-1"><a class="header-anchor" href="#将http请求转成https"><span>将http请求转成https</span></a></h1><p><strong>案例:将http:80请求转成https:443</strong><br><strong>仅适用于默认端口</strong></p><div class="language-confg line-numbers-mode" data-highlighter="shiki" data-ext="confg" data-title="confg" style="--shiki-light:#4c4f69;--shiki-dark:#E6E6E6;--shiki-light-bg:#eff1f5;--shiki-dark-bg:#222222;"><pre class="shiki shiki-themes catppuccin-latte slack-dark vp-code"><code><span class="line"><span>server {</span></span>
+<span class="line"><span>    listen       443; </span></span>
+<span class="line"><span>    server_name  localhost;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    #启用ssl</span></span>
+<span class="line"><span>    ssl on;</span></span>
+<span class="line"><span>    #ssl证书</span></span>
+<span class="line"><span>    ssl_certificate  /home/dyg/.ssl/cert.crt;</span></span>
+<span class="line"><span>    #ssl证书私钥</span></span>
+<span class="line"><span>    ssl_certificate_key /home/dyg/.ssl/cert-key.key;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    location / {</span></span>
+<span class="line"><span>        #反向代理</span></span>
+<span class="line"><span>        proxy_pass http://localhost:8081;</span></span>
+<span class="line"><span>        #默认页面</span></span>
+<span class="line"><span>        index  index.html index.htm;</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span>}</span></span>
+<span class="line"><span>server {</span></span>
+<span class="line"><span>    listen 80;</span></span>
+<span class="line"><span>    server_name localhost;</span></span>
+<span class="line"><span>    #将http请求转成https</span></span>
+<span class="line"><span>    rewrite ^(.*)$ https://$host$1 permanent;</span></span>
+<span class="line"><span>}</span></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,5),p=[l];function t(c,r){return a(),n("div",null,p)}const o=s(i,[["render",t],["__file","nginx实现反向代理并启用ssl的最简配置.html.vue"]]),m=JSON.parse('{"path":"/%E5%90%8E%E7%AB%AF/nginx%E5%AE%9E%E7%8E%B0%E5%8F%8D%E5%90%91%E4%BB%A3%E7%90%86%E5%B9%B6%E5%90%AF%E7%94%A8ssl%E7%9A%84%E6%9C%80%E7%AE%80%E9%85%8D%E7%BD%AE/nginx%E5%AE%9E%E7%8E%B0%E5%8F%8D%E5%90%91%E4%BB%A3%E7%90%86%E5%B9%B6%E5%90%AF%E7%94%A8ssl%E7%9A%84%E6%9C%80%E7%AE%80%E9%85%8D%E7%BD%AE.html","title":"nginx实现反向代理并启用ssl的最简配置","lang":"zh-CN","frontmatter":{"title":"nginx实现反向代理并启用ssl的最简配置","date":"2022-08-23T09:36:00.000Z","cover":"./cover/default_cover.jpg","tag":["nginx","反向代理"],"category":"笔记","description":"nginx实现反向代理并启用ssl的最简配置 将http请求转成https 案例:将http:80请求转成https:443 仅适用于默认端口","head":[["meta",{"property":"og:url","content":"https://dingdingdang.online/%E5%90%8E%E7%AB%AF/nginx%E5%AE%9E%E7%8E%B0%E5%8F%8D%E5%90%91%E4%BB%A3%E7%90%86%E5%B9%B6%E5%90%AF%E7%94%A8ssl%E7%9A%84%E6%9C%80%E7%AE%80%E9%85%8D%E7%BD%AE/nginx%E5%AE%9E%E7%8E%B0%E5%8F%8D%E5%90%91%E4%BB%A3%E7%90%86%E5%B9%B6%E5%90%AF%E7%94%A8ssl%E7%9A%84%E6%9C%80%E7%AE%80%E9%85%8D%E7%BD%AE.html"}],["meta",{"property":"og:site_name","content":"YiguiDing的Blog小站"}],["meta",{"property":"og:title","content":"nginx实现反向代理并启用ssl的最简配置"}],["meta",{"property":"og:description","content":"nginx实现反向代理并启用ssl的最简配置 将http请求转成https 案例:将http:80请求转成https:443 仅适用于默认端口"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2023-11-17T13:06:31.000Z"}],["meta",{"property":"article:author","content":"丁毅桂"}],["meta",{"property":"article:tag","content":"nginx"}],["meta",{"property":"article:tag","content":"反向代理"}],["meta",{"property":"article:published_time","content":"2022-08-23T09:36:00.000Z"}],["meta",{"property":"article:modified_time","content":"2023-11-17T13:06:31.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"nginx实现反向代理并启用ssl的最简配置\\",\\"image\\":[\\"\\"],\\"datePublished\\":\\"2022-08-23T09:36:00.000Z\\",\\"dateModified\\":\\"2023-11-17T13:06:31.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"丁毅桂\\",\\"email\\":\\"2449695354@qq.com\\"}]}"],["meta",{"name":"baidu-site-verification","content":"codeva-PwE9Ts6nMl"}]]},"headers":[],"git":{"createdTime":1700226391000,"updatedTime":1700226391000,"contributors":[{"name":"YiguiDing","email":"2449695354@qq.com","commits":1}]},"readingTime":{"minutes":0.63,"words":188},"filePathRelative":"后端/nginx实现反向代理并启用ssl的最简配置/nginx实现反向代理并启用ssl的最简配置.md","localizedDate":"2022年8月23日","excerpt":"","autoDesc":true}');export{o as comp,m as data};
