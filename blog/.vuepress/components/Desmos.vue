5edc96d3a9176e01d91c7260312f804335a502cc
YiguiDing <2449695354@qq.com>
2025-01-05 16:48:25 +0800

    update: 卡尔曼滤波器

--- blog/.vuepress/components/Desmos.vue
+++ blog/.vuepress/components/Desmos.vue
@@ -1,16 +1,30 @@
 <template>
-  <div ref="desmosElRef" class="desmosElRef"></div>
+  <div ref="wrapper" class="desmosElRef"></div>
+  <!-- <button @click="console.log(calculator.getState())">getState</button> -->
 </template>
 <script lang="ts" setup>
 import { ref, defineProps, onMounted, toValue } from "vue";
-const { expressions } = defineProps<{ expressions?: Array<{ id: string; latex: string }> }>();
-const desmosElRef = ref<HTMLDivElement>();
+const { state, expressions } = defineProps<{
+  state?: object;
+  expressions?: Array<{ id: string; latex: string }>;
+}>();
+const wrapper = ref<HTMLDivElement>();
+const calculator = ref<any>(undefined);
 onMounted(async () => {
+  // ########################################################################
+  // import desmos method 1: import from npm/desmos@1.5.4
   // const Desmos = await import("desmos");
-  const Desmos = await import("./calculator.js");
-  const calculator = Desmos.GraphingCalculator(toValue(desmosElRef)!);
+  // ########################################################################
+  // const calculator = Desmos.GraphingCalculator(toValue(desmosElRef)!);
+  // import desmos method 2: import desmos@v1.10.1
+  await import("./calculator.js");
+  calculator.value = window.Desmos.GraphingCalculator(toValue(wrapper)!);
+  // ########################################################################
+  // setState
+  state && calculator.value.setState(state);
+  // setExpression
   expressions?.forEach((expression) => {
-    calculator.setExpression(expression);
+    calculator.value.setExpression(expression);
   });
 });
 </script>
